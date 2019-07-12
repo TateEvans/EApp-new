111074571b5d599a3154b75a0fcf11c63da5ad4d
XHQ <>
2019-07-12 11:14:12 +0900

    no message

---
+++ ExperimentApp/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  ExperimentApp
+//
+//  Created by XHQ on 2019/7/5.
+//  Copyright © 2019 XHQ. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().tintColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        
+//        layoutTabBarItems()
+
+    }
+    
+//    func layoutTabBarItems () {
+    
+//        UITabBarItem.appearance().imageInsets = UIEdgeInsets(top: 3.0, left: 0.0, bottom: -3.0, right: 0.0)
+//        UITabBarItem.appearance().titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 3)
+        
+//    }
+    
+}
