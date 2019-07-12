6b38a30162bed9200054965d82e4a764b1a85142
XHQ <>
2019-07-12 11:05:44 +0900

    no message

--- ExperimentApp/ExperimentViewController.swift
+++ ExperimentApp/ExperimentViewController.swift
@@ -16,19 +16,10 @@ class ExperimentViewController: UIViewController, UITabBarDelegate  {
     
     @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var tabBar: UITabBar!
-    @IBOutlet weak var recentItem: UITabBarItem!
-    @IBOutlet weak var publishedItem: UITabBarItem!
-    @IBOutlet weak var chatItem: UITabBarItem!
-    @IBOutlet weak var profileItem: UITabBarItem!
-    
+    @IBOutlet weak var plusButton: UIButton!
     @IBAction func informaitonPublish(_ sender: UIButton) {
     }
     
-    
-    @IBOutlet weak var floatingButton: UIButton!
-    @IBAction func floatingButton(_ sender: UIButton) {
-    }
     
     
     override func viewDidLoad() {
@@ -39,29 +30,25 @@ class ExperimentViewController: UIViewController, UITabBarDelegate  {
         
 //        self.tableView.delegate = self
 //        self.tableView.dataSource = self
-        self.tabBar.delegate = self
+//        self.tabBar.delegate = self
 
-        var insets = UIEdgeInsets()
-        insets.top = 3
-        insets.bottom = -3.0
+//        var insets = UIEdgeInsets()
+//        insets.top = 3
+//        insets.bottom = -3.0
+//
+//        self.recentItem.imageInsets = insets
+//        self.recentItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 3)
+//        self.publishedItem.imageInsets = UIEdgeInsets(top: 3, left: -1, bottom: -3, right: 1)
+//        self.publishedItem.titlePositionAdjustment = UIOffset(horizontal: -27, vertical: 3)
+//        self.chatItem.imageInsets = UIEdgeInsets(top: 3, left: 1, bottom: -3, right: -1)
+//        self.chatItem.titlePositionAdjustment = UIOffset(horizontal: 27, vertical: 3)
+//        self.profileItem.imageInsets = insets
+//        self.profileItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 3)
         
-        self.recentItem.imageInsets = insets
-        self.recentItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 3)
-        self.publishedItem.imageInsets = insets
-        self.publishedItem.titlePositionAdjustment = UIOffset(horizontal: -27, vertical: 3)
-        self.chatItem.imageInsets = insets
-        self.chatItem.titlePositionAdjustment = UIOffset(horizontal: 27, vertical: 3)
-        self.profileItem.imageInsets = insets
-        self.profileItem.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 3)
-        
-        self.publishedItem.imageInsets = UIEdgeInsets(top: 0, left: -1, bottom: 0, right: 1)
-        self.chatItem.imageInsets = UIEdgeInsets(top: 0, left: 1, bottom: 0, right: -1)
-        
-        tabBar.tintColor = #colorLiteral(red: 0.09080412239, green: 0.2870057821, blue: 0.5362405777, alpha: 1)
-        tabBar.selectedItem = tabBar.items?.first
-        
-        self.floatingButton.alpha = 0.8
+//        tabBar.tintColor = #colorLiteral(red: 0.09080412239, green: 0.2870057821, blue: 0.5362405777, alpha: 1)
+//        tabBar.selectedItem = tabBar.items?.first
     
+        self.plusButton.alpha = 0.8
         
     }
     
